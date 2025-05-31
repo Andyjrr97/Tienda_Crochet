81bddaba6e99321d1728bc2f724d917f6c843f66
Josue97 <josue.rodriguez97.ar@gmail.com>
2025-05-31 15:42:15 -0600

    Creacion de la pagina web de Crochet, se crearon 8 paginas. se creo, animales,anime,contactenos,flores,index,informacion, peliculas y ubicacion, se cargaron las imagenes en la carpeta static.img

---
+++ src/main/java/com/tienda_crochet/TiendaCrochetApplication.java
@@ -0,0 +1,13 @@
+package com.tienda_crochet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaCrochetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaCrochetApplication.class, args);
+	}
+
+}
